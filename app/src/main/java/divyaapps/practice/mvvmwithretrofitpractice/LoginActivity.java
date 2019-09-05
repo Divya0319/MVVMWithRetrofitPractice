92a2dd055a83b24e9b2bee6aba2183620ece7636
divya0319 <divygupta0319@gmail.com>
2019-09-05 11:39:06 +0530

    Initial commit

---
+++ app/src/main/java/divyaapps/practice/mvvmwithretrofitpractice/LoginActivity.java
@@ -0,0 +1,14 @@
+package divyaapps.practice.mvvmwithretrofitpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
